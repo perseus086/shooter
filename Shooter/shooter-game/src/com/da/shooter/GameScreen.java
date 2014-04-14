f2c716eb4d2dbbdd0a6a8f9346f5b0791b7dff9b
Paul Hinojosa <perseus086@gmail.com>
2014-04-14 18:58:07 +1000

    Agregado Game screen

---
+++ Shooter/shooter-game/src/com/da/shooter/GameScreen.java
@@ -0,0 +1,50 @@
+package com.da.shooter;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen {
+
+	private Game game;
+	
+	public GameScreen(Game game){
+		this.game = game;
+		System.out.println("Game Screen");
+	}
+	
+	@Override
+	public void render(float delta) {
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
--- Shooter/shooter-game/src/com/da/shooter/MenuScreen.java
+++ Shooter/shooter-game/src/com/da/shooter/MenuScreen.java
@@ -3,6 +3,7 @@ package com.da.shooter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL30;
 
 public class MenuScreen implements Screen{
@@ -19,6 +20,10 @@ public class MenuScreen implements Screen{
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 		
+		if(Gdx.input.isTouched() || Gdx.input.isKeyPressed(Keys.ENTER))
+		{
+			game.setScreen(new GameScreen(game));
+		}
 	}
 
 	@Override
